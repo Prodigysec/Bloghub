16fabd4926254d4221b6e880d394fc8d53b5d594
Ian Raburu <williamraburu@gmail.com>
2023-10-06 18:11:21 +0300

    fetch blogs from the posts endpoint and pass to Bloglist component

--- client/src/components/blogs.jsx
+++ client/src/components/blogs.jsx
@@ -1,15 +1,35 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import Bloglist from './blogList';
 
 function Blogs() {
-    // make request to blogs endpoint
-    fetch("/api/blogs", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-    .then((r) => r.json())
-    .then((data) => {
-        console.log(data);
-    });
+    const [blogs, setBlogs] = useState("");
+
+    useEffect(() => {
+        // make request to blogs endpoint
+        fetch("/api/posts", {
+            method: "GET",
+        })
+        .then((res) => {
+            if (res.redirected) {
+                setTimeout(() => {
+                    window.location.href = res.url;
+                }, 2000);
+            } else {
+                return res.json();
+            }
+        })
+        .then((data) => {
+            setBlogs(data);
+
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1>Blogs</h1>
+            <Bloglist blogs={blogs} />
+        </div>
+    );
 }
+
+export default Blogs;
