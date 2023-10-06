a66388851276f269759257811b601b3fd3d838f9
Ian Raburu <williamraburu@gmail.com>
2023-10-06 18:07:31 +0300

    Add routes to different endpoints

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
+import Signup from './components/signup'
+import Signin from './components/signin'
+import Blogs from './components/blogs'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/home'
 
 function App() {
-
   return (
-    <>
-      <h1>Bloghub App</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/> } />
+        <Route path='/signup' element={<Signup/> } />
+        <Route path='/login' element={<Signin/> } />
+        <Route path='/blogs' element={<Blogs/> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
