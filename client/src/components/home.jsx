98ad583be509b5c89e16241422083e9ee53b8db6
Ian Raburu <williamraburu@gmail.com>
2023-10-07 23:51:04 +0300

    Create home page

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,10 +1,20 @@
 import React, {useState} from 'react';
+import '../componentStyles/home.css';
 
 function Home() {
   return (
     <div>
-      <h2>Home</h2>
-      <p>Home page</p>
+      <header>
+        <h2>Bloghub</h2>
+        <nav>
+          <ul>
+            <li><a href="/about">About</a></li>
+            <li><a href="/blogs">Blogs</a></li>
+            <li><a href="/signup">SignUp</a></li>
+          </ul>
+        </nav>
+      </header>
+      <p className='quote'>Express your thoughts</p>
     </div>
   )
 }
